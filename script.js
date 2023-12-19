dcac4971e52209acfef23fc476d9d92cd97b9863
Mohamed_Nihmathullah <106542888+MohamedNihmath18@users.noreply.github.com>
2023-12-19 19:01:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+setTimeout(() => {
+    document.body.innerHTML = "<span style='color:red;'>10</span>";
+    setTimeout(() => {
+        document.body.innerHTML = "<span style='color:red;'>9</span>";
+        setTimeout(() => {
+            document.body.innerHTML = "<span style='color:red;'>8</span>";
+            setTimeout(() => {
+                document.body.innerHTML = "<span style='color:red;'>7</span>";
+                setTimeout(() => {
+                    document.body.innerHTML = "<span style='color:red;'>6</span>";
+                    setTimeout(() => {
+                        document.body.innerHTML = "<span style='color:red;'>5</span>";
+                        setTimeout(() => {
+                            document.body.innerHTML = "<span style='color:red;'>4</span>";
+                            setTimeout(() => {
+                                document.body.innerHTML = "<span style='color:red;'>3</span>";
+                                setTimeout(() => {
+                                    document.body.innerHTML = "<span style='color:red;'>2</span>";
+                                    setTimeout(() => {
+                                        document.body.innerHTML = "<span style='color:red;'>1</span>";
+                                        setTimeout(() => {
+                                            document.body.innerHTML = `
+                                            <b><h1 style="color:red;">
+                                                Happy Independence Day!
+                                            </h1></b>`;
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
